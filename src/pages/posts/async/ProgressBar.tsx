750e546a3ae855a95dddda58eb248edf2d6a1264
Conrad Ludgate <conrad.ludgate@truelayer.com>
2022-05-11 12:13:58 +0100

    add progressbar joke

---
+++ src/pages/posts/async/ProgressBar.tsx
@@ -0,0 +1,42 @@
+import styles from "./ProgressBar.module.css";
+import { ReactElement, useEffect, useMemo, useState } from "react";
+
+export default function ProgressBar(): ReactElement {
+	const [counter, setCounter] = useState(0);
+
+	useEffect(() => {
+		const timer = setInterval(() => {
+			setCounter(c => c + 1);
+		}, 200);
+		return () => clearInterval(timer);
+	}, [setCounter]);
+
+	const [widthInt, setWidthInt] = useState(0);
+
+	const width = useMemo(() => {
+		const width = (1 - Math.exp(counter / -100)) * 100;
+		setWidthInt(Math.floor(width));
+		return width;
+	}, [counter, setWidthInt]);
+
+	const joke = useMemo(() => {
+		return jokes[Math.floor(Math.random() * jokes.length)];
+	}, [widthInt]);
+
+	return (
+		<div className={styles.ProgressBar_Container}>
+			<span className={styles.ProgressBar_Joke}>
+				{joke}
+			</span>
+			<div className={styles.ProgressBar}>
+				<div className={styles.ProgressBar_Bar} style={{ width: `${width}%` }}></div>
+			</div>
+		</div>
+	);
+}
+
+const jokes = [
+	"Mining crypto",
+	"Stealing cookies",
+	"Leaving the toilet seat up",
+];

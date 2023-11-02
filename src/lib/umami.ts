0b1a2e5642f704d405c6da9b3b0ea5b980b568ce
Conrad Ludgate <conradludgate@gmail.com>
2023-11-02 16:58:26 +0000

    fix

--- src/lib/umami.ts
+++ src/lib/umami.ts
@@ -1,17 +1,20 @@
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export function useTracking(): void {
-	const {
-		location: { pathname, search },
-	} = window;
-	let currentUrl = `${pathname}${search}`;
-	let currentRef = document.referrer;
+	const [currentUrl, setCurrentUrl] = useState("");
+	const [currentRef, setCurrentRef] = useState("");
 	const router = useRouter();
 	useEffect(() => {
+		const {
+			location: { pathname, search },
+		} = window;
+		if (!currentUrl) { setCurrentUrl(`${pathname}${search}`); }
+		if (!currentRef) { setCurrentRef(document.referrer); }
+
 		const handleRouteChange = (url: string) => {
-			currentRef = currentUrl;
-			currentUrl = url.toString();
+			setCurrentRef(currentUrl);
+			setCurrentUrl(url.toString());
 
 			if (currentUrl !== currentRef) {
 				setTimeout(() => send(getPayload(currentUrl, currentRef)), delayDuration);
@@ -19,42 +22,43 @@ export function useTracking(): void {
 		};
 		router.events.on("beforeHistoryChange", handleRouteChange);
 		return () => { router.events.off("beforeHistoryChange", handleRouteChange); };
-	}, [router.events]);
+	}, [router.events, currentRef, currentUrl, setCurrentRef, setCurrentUrl]);
 }
 
 const website = process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID;
 const hostURL = process.env.NEXT_PUBLIC_UMAMI_HOST_URL;
 
-const {
-	screen: { width, height },
-	navigator: { language },
-	location,
-	localStorage,
-	document,
-} = window;
-const { hostname } = location;
-
 const ignoredDomains: string[] = ["localhost:3000"];
-const screen = `${width}x${height}`;
 const delayDuration = 300;
 
-const getPayload = (currentUrl: string, currentRef: string) => ({
-	website,
-	hostname,
-	screen,
-	language,
-	title: window.document.title,
-	url: currentUrl,
-	referrer: currentRef,
-});
+const getPayload = (currentUrl: string, currentRef: string) => {
+	const {
+		screen: { width, height },
+		navigator: { language },
+		location,
+	} = window;
+	const { hostname } = location;
+	const screen = `${width}x${height}`;
+
+	return {
+		website,
+		hostname,
+		screen,
+		language,
+		title: window.document.title,
+		url: currentUrl,
+		referrer: currentRef,
+	};
+};
 
 /* Tracking functions */
 
+/* eslint @typescript-eslint/ban-ts-comment: "off" */
 const doNotTrack: () => boolean = () => {
 	// @ts-ignore
 	const { doNotTrack, navigator, external } = window;
 
-	const msTrackProtection = 'msTrackingProtectionEnabled';
+	const msTrackProtection = "msTrackingProtectionEnabled";
 	const msTracking = () => {
 		// @ts-ignore
 		return external && msTrackProtection in external && external[msTrackProtection]();
@@ -63,29 +67,32 @@ const doNotTrack: () => boolean = () => {
 	// @ts-ignore
 	const dnt = doNotTrack || navigator.doNotTrack || navigator.msDoNotTrack || msTracking();
 
-	return dnt == '1' || dnt === 'yes';
+	return dnt == "1" || dnt === "yes";
 };
 
 const trackingDisabled = () =>
-	(localStorage && localStorage.getItem('umami.disabled')) ||
+	(window.localStorage && window.localStorage.getItem("umami.disabled")) ||
 	doNotTrack() ||
-	(!ignoredDomains.includes(hostname));
+	(!ignoredDomains.includes(window.location.hostname));
 
 let cache: string | undefined = undefined;
-const send: (payload: any, type?: string) => Promise<string | void> = (payload, type = 'event') => {
+
+/* eslint @typescript-eslint/no-explicit-any: "off" */
+const send: (payload: any, type?: string) => Promise<string | void> = (payload, type = "event") => {
 	if (trackingDisabled()) return Promise.resolve();
 	const headers: Record<string, string> = {
-		'Content-Type': 'application/json',
+		"Content-Type": "application/json",
 	};
-	if (typeof cache !== 'undefined') {
-		headers['x-umami-cache'] = cache;
+	if (typeof cache !== "undefined") {
+		headers["x-umami-cache"] = cache;
 	}
 	return fetch(`${hostURL}/api/send`, {
-		method: 'POST',
+		method: "POST",
 		body: JSON.stringify({ type, payload }),
 		headers,
 	})
 		.then(res => res.text())
 		.then(text => (cache = text))
+		/* eslint @typescript-eslint/no-empty-function: "off" */
 		.catch(() => { }); // no-op, gulp error
 };
